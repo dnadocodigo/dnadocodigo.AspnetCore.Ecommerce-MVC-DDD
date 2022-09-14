e6ba2f5a58d650cc47697a9ebc23919be3a2a7e4
developer's Brasil DBA <dnadevnet@gmail.com>
2022-09-14 03:19:14 -0300

    Add CLass Base

---
+++ src/Entity/Entities/Generics/Base.cs
@@ -0,0 +1,17 @@
+﻿using Entity.Notifications;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Entity.Entities.Generics
+{
+    public class Base : Notify
+    {
+        [Display(Name="Código")]
+        public int Code { get; set; }
+
+        [Display(Name = "Nome")]
+        public string Name { get; set; }
+    }
+}
