1ec5f418cd3e9a1d21a82bfbcfdfac7d7df947ee
developer's Brasil DBA <dnadevnet@gmail.com>
2022-09-14 03:55:32 -0300

    Add Interface Product Service

---
+++ src/Domain/Interfaces/IServices/Products/IProductService.cs
@@ -0,0 +1,15 @@
+﻿using Entity.Entities.Products;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces.IServices.Products
+{
+    public interface IProductService
+    {
+        //Custumizados
+        Task ProductAdd(Product product);
+        Task ProductUpdate(Product product);
+    }
+}
