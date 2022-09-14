ddba52000a32dc1bf01c954023a36b30cf8c19d3
developer's Brasil DBA <dnadevnet@gmail.com>
2022-09-14 03:02:20 -0300

    Add Notify Entity

---
+++ src/Entity/Notifications/Notify.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entity.Notifications
+{
+    public class Notify
+    {
+        public Notify()
+        {
+            Notification = new List<Notify>();
+        }
+
+        //Propriedades===============================================
+
+        [NotMapped]
+        public string PropertyName { get; set; }
+
+        [NotMapped]
+        public string Message { get; set; }
+
+        [NotMapped]
+        public List<Notify> Notification;
+
+        //Métodos ===================================================
+
+        // Validações
+        public bool StringPropertyValidations(string value, string propertiesName)
+        {
+            if (string.IsNullOrWhiteSpace(value) || string.IsNullOrWhiteSpace(propertiesName))
+            {
+                Notification.Add(new Notify
+                {
+                    Message = "Este campo é obrigatório",
+                    PropertyName = propertiesName
+                });
+                return false;
+            }
+            return true;
+        }
+
+        public bool IntPropertyValidations(int value, string propertiesName)
+        {
+            if (value < 1 || string.IsNullOrWhiteSpace(propertiesName))
+            {
+                Notification.Add(new Notify
+                {
+                    Message = "O valor tem que ser maior que 0!",
+                    PropertyName = propertiesName
+                });
+                return false;
+            }
+            return true;
+        }
+
+        public bool DecimalPropertyValidations(decimal value, string propertiesName)
+        {
+            if (value < 1 || string.IsNullOrWhiteSpace(propertiesName))
+            {
+                Notification.Add(new Notify
+                {
+                    Message = "O valor tem que ser maior que 0!",
+                    PropertyName = propertiesName
+                });
+                return false;
+            }
+            return true;
+        }
+
+    }
+}
--- src/UI.Web/Models/ErrorViewModel.cs
+++ src/UI.Web/Models/ErrorViewModel.cs
@@ -1,5 +1,3 @@
-using System;
-
 namespace UI.Web.Models
 {
     public class ErrorViewModel
