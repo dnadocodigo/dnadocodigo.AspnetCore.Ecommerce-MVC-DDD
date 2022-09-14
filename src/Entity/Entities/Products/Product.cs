f3c4b23992a0df6268032cc9a30f5003b6f3939c
developer's Brasil DBA <dnadevnet@gmail.com>
2022-09-14 03:29:32 -0300

    Add Product Calss

---
+++ src/Entity/Entities/Products/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Entity.Entities.Products
+{
+    public class Product
+    {
+        [Column("CODIGO")]
+        [Display(Name="Código")]
+        public int Id { get; set; }
+
+        [Column("NOME")]
+        [Display(Name = "Nome")]
+        public string Name { get; set; }
+
+        [Column("VALOR")]
+        [Display(Name = "Valor")]
+        public decimal Value { get; set; }
+
+        [Column("ESTADO")]
+        [Display(Name = "Estado")]
+        public bool Status { get; set; }
+    }
+}
